f6618c13749c64f8246cae9a652439050e2dbc09
cboudereau <cboudereau@gmail.com>
2018-07-09 20:12:14 +0200

    add int parsing

--- parsing.fs
+++ parsing.fs
@@ -2,6 +2,11 @@ module Parsing
 
 open System.Text.RegularExpressions
 
+let (|Int|_|) x = 
+    match System.Int32.TryParse(x) with
+    | true, i -> Some i
+    | _ -> None
+
 let (|Regex|_|) pattern source = 
     let m = Regex.Match(source, pattern, RegexOptions.CultureInvariant ||| RegexOptions.IgnoreCase)
     if m.Success then [ for x in m.Groups -> x.Value ] |> List.tail |> Some
